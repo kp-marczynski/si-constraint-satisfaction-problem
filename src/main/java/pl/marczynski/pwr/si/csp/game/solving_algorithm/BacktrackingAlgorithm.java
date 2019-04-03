de9cb2fc0c44c191401fcfe5ed23d12ce638a755
kp-marczynski <kp.marczynski@gmail.com>
2019-04-04 00:26:58 +0200

    implemented solving algorithms

--- src/main/java/pl/marczynski/pwr/si/csp/game/solving_algorithm/BacktrackingAlgorithm.java
+++ src/main/java/pl/marczynski/pwr/si/csp/game/solving_algorithm/BacktrackingAlgorithm.java
@@ -1,4 +1,17 @@
 package pl.marczynski.pwr.si.csp.game.solving_algorithm;
 
-public class BacktrackingAlgorithm {
+import pl.marczynski.pwr.si.csp.board.Board;
+import pl.marczynski.pwr.si.csp.board.FieldId;
+
+public class BacktrackingAlgorithm implements SolvingAlgorithm {
+    @Override
+    public Board initializeBoard(String fileName) {
+        return Board.initializeFromFile(fileName);
+    }
+
+    @Override
+    public Board makeMove(Board board, FieldId fieldId, Integer value) {
+        board.makeMove(fieldId, value);
+        return board;
+    }
 }
--- src/main/java/pl/marczynski/pwr/si/csp/game/solving_algorithm/ForwardCheckingAlgoritm.java
+++ src/main/java/pl/marczynski/pwr/si/csp/game/solving_algorithm/ForwardCheckingAlgoritm.java
@@ -1,4 +1,20 @@
 package pl.marczynski.pwr.si.csp.game.solving_algorithm;
 
-public class ForwardCheckingAlgoritm {
+import pl.marczynski.pwr.si.csp.board.Board;
+import pl.marczynski.pwr.si.csp.board.FieldId;
+
+public class ForwardCheckingAlgoritm implements SolvingAlgorithm {
+    @Override
+    public Board initializeBoard(String fileName) {
+        Board board = Board.initializeFromFile(fileName);
+        board.initializeNullsWithPossibleValues();
+        return board;
+    }
+
+    @Override
+    public Board makeMove(Board board, FieldId fieldId, Integer value) {
+        board.makeMove(fieldId, value);
+        board.removeAllForbiddenValues();
+        return board;
+    }
 }

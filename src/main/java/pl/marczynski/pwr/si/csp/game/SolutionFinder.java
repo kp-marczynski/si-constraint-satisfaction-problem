0c62012acf4db9135cc12fbe2e88d163e5d5f629
kp-marczynski <kp.marczynski@gmail.com>
2019-04-04 00:28:14 +0200

    implemented solving algorithms

--- src/main/java/pl/marczynski/pwr/si/csp/game/SolutionFinder.java
+++ src/main/java/pl/marczynski/pwr/si/csp/game/SolutionFinder.java
@@ -1,12 +1,21 @@
 package pl.marczynski.pwr.si.csp.game;
 
 import pl.marczynski.pwr.si.csp.board.FieldId;
+import pl.marczynski.pwr.si.csp.game.heuristics.Heuristics;
+import pl.marczynski.pwr.si.csp.game.solving_algorithm.SolvingAlgorithm;
 
 import java.util.List;
 
 public class SolutionFinder {
     FieldId currentRoot;
     List<Integer> valuesToCheck;
+    SolvingAlgorithm solvingAlgorithm;
+    Heuristics heuristics;
 
-
+    public SolutionFinder(FieldId currentRoot, List<Integer> valuesToCheck, SolvingAlgorithm solvingAlgorithm, Heuristics heuristics) {
+        this.currentRoot = currentRoot;
+        this.valuesToCheck = valuesToCheck;
+        this.solvingAlgorithm = solvingAlgorithm;
+        this.heuristics = heuristics;
+    }
 }

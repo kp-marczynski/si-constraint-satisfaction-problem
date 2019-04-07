630642a331ab652abd9745b367fb79e545d16e0f
kp-marczynski <kp.marczynski@gmail.com>
2019-04-07 23:25:02 +0200

    tests improvements

--- src/main/java/pl/marczynski/pwr/si/csp/Main.java
+++ src/main/java/pl/marczynski/pwr/si/csp/Main.java
@@ -2,31 +2,30 @@ package pl.marczynski.pwr.si.csp;
 
 import pl.marczynski.pwr.si.csp.board.Board;
 import pl.marczynski.pwr.si.csp.board.FieldId;
+import pl.marczynski.pwr.si.csp.game.SolutionCollection;
+import pl.marczynski.pwr.si.csp.game.SolutionFinder;
+import pl.marczynski.pwr.si.csp.game.heuristics.FifoHeuristics;
+import pl.marczynski.pwr.si.csp.game.heuristics.Heuristics;
+import pl.marczynski.pwr.si.csp.game.solving_algorithm.BacktrackingAlgorithm;
+import pl.marczynski.pwr.si.csp.game.solving_algorithm.ForwardCheckingAlgorithm;
+import pl.marczynski.pwr.si.csp.game.solving_algorithm.SolvingAlgorithm;
 
-import java.util.Scanner;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Main {
-
+    private static final String DATA_PATH = "./src/main/resources/";
 
     public static void main(String[] args) {
-//        List<SolvingAlgorithm> solvingAlgorithms = Arrays.asList(new BacktrackingAlgorithm(), new ForwardCheckingAlgorithm());
-//        List<Heuristics> heuristics = Arrays.asList(new FifoHeuristics());
-//        String directory = "research_data";
-////        List<String> fileNames = getFilesInDirectory(directory);
-//        List<String> fileNames = Collections.singletonList("test_futo_4_0.txt");
-//
-//        List<SolutionCollection> allSolutions = new ArrayList<>();
-//        for (String fileName : fileNames) {
-//            for (SolvingAlgorithm solvingAlgorithm : solvingAlgorithms) {
-//                for (Heuristics heuristic : heuristics) {
-//                    SolutionFinder solutionFinder = new SolutionFinder(solvingAlgorithm, heuristic, directory + "/" + fileName);
-//
-//                    SolutionCollection solutionCollection = solutionFinder.findSolution();
-//                    allSolutions.add(solutionCollection);
-//                    System.out.println(solutionCollection);
-//                }
-//            }
-//        }
+        String directory = "research_data";
+//        List<String> fileNames = getFilesInDirectory(directory);
+        List<String> fileNames = Collections.singletonList("test_futo_4_0.txt");
+        findSolutionsforFiles(directory, fileNames);
     }
 
     public static void manualRun() {
@@ -42,5 +41,36 @@ public class Main {
             board.makeMove(new FieldId(row, col), val);
             System.out.println(board);
         }
+    }
+
+    static List<SolutionCollection> findSolutionsforFiles(String directory, List<String> fileNames) {
+        List<SolvingAlgorithm> solvingAlgorithms = Arrays.asList(new BacktrackingAlgorithm(), new ForwardCheckingAlgorithm());
+        List<Heuristics> heuristics = Arrays.asList(new FifoHeuristics());
+
+        List<SolutionCollection> allSolutions = new ArrayList<>();
+        for (String fileName : fileNames) {
+            for (SolvingAlgorithm solvingAlgorithm : solvingAlgorithms) {
+                for (Heuristics heuristic : heuristics) {
+                    SolutionFinder solutionFinder = new SolutionFinder(solvingAlgorithm, heuristic, directory + "/" + fileName);
+
+                    SolutionCollection solutionCollection = solutionFinder.findSolution();
+                    allSolutions.add(solutionCollection);
+                    System.out.println(solutionCollection);
+                }
+            }
+        }
+        return allSolutions;
+    }
+
+    static List<String> getFilesInDirectory(String directory) {
+        List<String> result = new ArrayList<>();
+        try (Stream<Path> walk = Files.walk(Paths.get(DATA_PATH + "/" + directory))) {
+
+            result = walk.filter(Files::isRegularFile).map(Path::getFileName)
+                    .map(Path::toString).collect(Collectors.toList());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return result;
     }
 }
--- src/test/java/pl/marczynski/pwr/si/csp/MainTest.java
+++ src/test/java/pl/marczynski/pwr/si/csp/MainTest.java
@@ -1,59 +1,25 @@
 package pl.marczynski.pwr.si.csp;
 
 import org.junit.Test;
-import pl.marczynski.pwr.si.csp.game.SolutionCollection;
-import pl.marczynski.pwr.si.csp.game.SolutionFinder;
-import pl.marczynski.pwr.si.csp.game.heuristics.FifoHeuristics;
-import pl.marczynski.pwr.si.csp.game.heuristics.Heuristics;
-import pl.marczynski.pwr.si.csp.game.solving_algorithm.BacktrackingAlgorithm;
-import pl.marczynski.pwr.si.csp.game.solving_algorithm.ForwardCheckingAlgorithm;
-import pl.marczynski.pwr.si.csp.game.solving_algorithm.SolvingAlgorithm;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class MainTest {
-
-    private static final String DATA_PATH = "./src/main/resources/";
-
+    
     @Test
-    public void test() {
-        List<SolvingAlgorithm> solvingAlgorithms = Arrays.asList(new BacktrackingAlgorithm(), new ForwardCheckingAlgorithm());
-        List<Heuristics> heuristics = Arrays.asList(new FifoHeuristics());
+    public void evaluateTestData() {
         String directory = "test_data";
-        List<String> fileNames = getFilesInDirectory(directory);
-//        List<String> fileNames = Collections.singletonList("test_futo_4_0.txt");
-
-        List<SolutionCollection> allSolutions = new ArrayList<>();
-        for (String fileName : fileNames) {
-            for (SolvingAlgorithm solvingAlgorithm : solvingAlgorithms) {
-                for (Heuristics heuristic : heuristics) {
-                    SolutionFinder solutionFinder = new SolutionFinder(solvingAlgorithm, heuristic, directory + "/" + fileName);
-
-                    SolutionCollection solutionCollection = solutionFinder.findSolution();
-                    allSolutions.add(solutionCollection);
-                    System.out.println(solutionCollection);
-                }
-            }
-        }
+        List<String> fileNames = Main.getFilesInDirectory(directory);
+        fileNames.sort(Comparator.naturalOrder());
+        Main.findSolutionsforFiles(directory, fileNames);
     }
 
-    private static List<String> getFilesInDirectory(String directory) {
-        List<String> result = new ArrayList<>();
-        try (Stream<Path> walk = Files.walk(Paths.get(DATA_PATH + "/" + directory))) {
-
-            result = walk.filter(Files::isRegularFile).map(Path::getFileName)
-                    .map(Path::toString).collect(Collectors.toList());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return result;
+    @Test
+    public void evaluateResearchData() {
+        String directory = "research_data";
+        List<String> fileNames = Main.getFilesInDirectory(directory);
+        fileNames.sort(Comparator.naturalOrder());
+        Main.findSolutionsforFiles(directory, fileNames);
     }
 }
